00d91feb45f464786e49541ae92a34ba9aa2b019
YKADOSK <kadosh23yael@gmail.com>
2025-04-22 10:22:37 +0300

    Create rectangle.py

---
+++ rectangle.py
@@ -0,0 +1,13 @@
+#rectangle primeter and area:
+height=input("height:")
+width=input("width:")
+h=int(height)
+w=int(width)
+
+#primeter:
+p=2*(h+w)
+print("The perimeter is " + str(p) +" mm")
+
+#area:
+a=h*w
+print("The area is "+ str(a) +" mm^2")
