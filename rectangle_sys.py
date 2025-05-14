1ceb59d414f13137d722ed56a8bbff8c74b37004
YKADOSK <kadosh23yael@gmail.com>
2025-05-14 14:00:46 +0300

    Create rectangle_sys.py

---
+++ rectangle_sys.py
@@ -0,0 +1,12 @@
+#Rectangle primeter and area:
+import sys
+h=int(sys.argv[1])
+w=int(sys.argv[2])
+
+#primeter:
+p=2*(h+w)
+print("The perimeter is " + str(p) +" mm")
+
+#area:
+a=h*w
+print("The area is "+ str(a) +" mm^2")
