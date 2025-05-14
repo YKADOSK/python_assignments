cc35ce7cb32cbc8afe005d72fbdf992d8495e058
YKADOSK <kadosh23yael@gmail.com>
2025-05-14 14:00:12 +0300

    Create circle_sys.py

---
+++ circle_sys.py
@@ -0,0 +1,12 @@
+import sys 
+
+r=float(sys.argv[1])
+pi=3.14
+
+#circumference:
+C=pi*r*2
+print("The circumference is " + str(C) + " mm")
+
+#Area:
+A=pi*r*r
+print("The area is " + str(A) + " mm^2") 
